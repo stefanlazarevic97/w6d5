863aeab945d82e68d4ac7765550b2235d8937e26
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-02 13:00:42 -0700

    add-new-view

--- 99-cats/ninety-nine-cats/app/controllers/cats_controller.rb
+++ 99-cats/ninety-nine-cats/app/controllers/cats_controller.rb
@@ -5,7 +5,7 @@ class CatsController < ApplicationController
     end
 
     def show
-        @cat = Cat.find_by(id: params[:id])
+        @cat = Cat.find(params[:id])
         render :show
     end
 
@@ -16,31 +16,33 @@ class CatsController < ApplicationController
 
     def create
         @cat = Cat.new(cat_params)
+
         if @cat.save
-            redirect_to cat_url(@cat.id)
+            redirect_to cat_url(@cat)
         else
+            flash.now[:errors] = @cat.errors.full_messages
             render :new
         end
     end
 
     def edit
-        @cat = Cat.find_by(id: params[:id])
+        @cat = Cat.find(params[:id])
         render :edit
     end
 
     def update
-        @cat = Cat.find_by(id: params[:id])
+        @cat = Cat.find(params[:id])
         if @cat.update(cat_params)
             redirect_to cat_url(@cat.id)
         else
+            flash.now[:errors] = @cat.errors.full_messages
             render :edit
         end
-    end
-
-    
+    end    
 
     private
+
     def cat_params
-        params.require(:cats).permit(:name, :color, :birth_date, :sex, :color, :description)
+        params.require(:cats).permit(:name, :color, :birth_date, :sex, :description)
     end
 end
--- 99-cats/ninety-nine-cats/config/routes.rb
+++ 99-cats/ninety-nine-cats/config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "cats#index"
-  resources :cats, except:[:destroy]
+    # Defines the root path route ("/")
+    # root "articles#index"
 
-end
+    root "cats#index"
+    
+    resources :cats, except:[:destroy] do
+        resources :cat_rental_requests, only: [:new, :create]
+    end
+
+    resources :cat_rental_requests, only: [:new, :create] do
+        member do
+        post :approve
+        post :deny
+        end
+    end
+    end
