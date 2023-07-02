d996e79372225ec0cd5a948060092f732db14616
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-02 13:35:09 -0700

    update-cat-params

--- 99-cats/ninety-nine-cats/app/controllers/cats_controller.rb
+++ 99-cats/ninety-nine-cats/app/controllers/cats_controller.rb
@@ -43,6 +43,6 @@ class CatsController < ApplicationController
     private
 
     def cat_params
-        params.require(:cats).permit(:name, :color, :birth_date, :sex, :description)
+        params.require(:cat).permit(:name, :color, :birth_date, :sex, :description)
     end
 end
